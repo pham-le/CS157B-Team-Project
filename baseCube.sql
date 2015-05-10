61d3911446df36cc60ef833904f4e3924287f524
glorio <glorioyulianto@airbears2-10-142-52-71.AirBears2.1918.Berkeley.EDU>
2015-05-09 20:21:52 -0700

    added comments

--- baseCube.sql
+++ baseCube.sql
@@ -1,4 +1,12 @@
+/*
+base cube: [table -> attribute]
+product -> category
+time -> month
+store -> store_state
+promotion -> ad_media_type
+*/
 SELECT t.Month, p.category, s.store_state, sum(sf.unit_sales) total_unit_sold, sum(sf.customer_count) customer_count, sum(sf.dollar_sales) revenue, (sum(sf.dollar_sales) - sum(sf.dollar_cost)) profit
 FROM Grocery.`Sales Fact` sf, Product p, Store s, Time t, Promotion pr
 where sf.product_key = p.product_key and sf.promotion_key = pr.promotion_key and sf.store_key = s.store_key and sf.time_key = t.time_key
 group by s.store_state, p.category, t.Month;
+
